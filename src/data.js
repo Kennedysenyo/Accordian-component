89e15e02554c006cb884cef0d016f9738d9005d9
Kennedy Senyo Dordoe <kennedysenyo@gmail.com>
2025-01-06 09:30:43 +0000

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const data = [
+  {
+    id: 1,
+    question: "What is React?",
+    answer: "React is a JavaScript library for building user interfaces."
+  },
+  {
+    id: 2,
+    question: "What is a component in React?",
+    answer: "A component is a reusable piece of UI that can have its own state and logic."
+  },
+  {
+    id: 3,
+    question: "What is state in React?",
+    answer: "State is a built-in object that stores dynamic data and controls a component's behavior."
+  }
+];
